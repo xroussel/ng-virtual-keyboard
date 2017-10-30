e0162643e07c61a3fd05827d82132239e83016e8
Adrien Manikon <adrien.manikon@stemys.io>
2017-10-30 09:23:54 +0100

    JEIX1-451 add compiled files

--- dist/ng-virtual-keyboard.umd.js
+++ dist/ng-virtual-keyboard.umd.js
@@ -94,6 +94,7 @@ exports.alphanumericKeyboardSwiss = [
     ['q', 'w', 'e', 'r', 't', 'z', 'u', 'i', 'o', 'p', 'CapsLock:2'],
     ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'Spacer', 'Enter:2'],
     ['y', 'x', 'c', 'v', 'b', 'n', 'm', 'Spacer:3', 'Accent:2'],
+    ['SpaceBar:12']
 ];
 exports.alphanumericKeyboardSwissAccent = [
     ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Backspace:2'],
