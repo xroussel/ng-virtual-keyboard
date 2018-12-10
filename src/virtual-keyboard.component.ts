f66a506b4a4f6652b8e8fa611d9fa51712e6423d
jeremy <jeremy.schneiter@kinitic.ch>
2018-12-10 14:54:14 +0100

    add physical 'ö' char

--- src/virtual-keyboard.component.ts
+++ src/virtual-keyboard.component.ts
@@ -371,6 +371,9 @@ export class VirtualKeyboardComponent implements OnInit, OnDestroy {
 			case 'Del':
 				this.handleNormalKey('.');
 				break;
+			case 'ö':
+				this.handleNormalKey('ö');
+				break;
 		}
 	}
 
@@ -447,7 +450,10 @@ export class VirtualKeyboardComponent implements OnInit, OnDestroy {
 		if($event.key == "Del") {
 			keyvalue = '.';
 		}
-
+		if($event.key == "ö") {
+			keyvalue = 'ö';
+		}
+		
 		var keyPressInterface:KeyPressInterface = {
 			key: ""+ $event.keyCode,
 			keyValue: keyvalue,
